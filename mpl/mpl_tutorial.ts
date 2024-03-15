1f46bbaeb9cf5ae511cff41be76c404ca56f26c1
GDOGCoin <163420667+GDOGCoin@users.noreply.github.com>
2024-03-15 14:23:31 +0200

    Update Metadata Updater

--- mpl/mpl_tutorial.ts
+++ mpl/mpl_tutorial.ts
@@ -1,9 +1,15 @@
-// as used in https://www.youtube.com/watch?v=DQbt0-riooo
-
-import * as mpl from "@metaplex-foundation/mpl-token-metadata";
+import { createV1, updateV1 ,Collection, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionDataArgs, Creator, MPL_TOKEN_METADATA_PROGRAM_ID, UpdateMetadataAccountV2InstructionAccounts, UpdateMetadataAccountV2InstructionData, Uses, createMetadataAccountV3, updateMetadataAccountV2, findMetadataPda, CreateV1InstructionAccounts, CreateV1InstructionData, TokenStandard, CollectionDetails, PrintSupply, UpdateV1InstructionData, UpdateV1InstructionAccounts, Data} from "@metaplex-foundation/mpl-token-metadata";
 import * as web3 from "@solana/web3.js";
-import * as anchor from '@project-serum/anchor';
+import { PublicKey, createSignerFromKeypair, none, percentAmount, publicKey, signerIdentity, some } from "@metaplex-foundation/umi";
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { fromWeb3JsKeypair, fromWeb3JsPublicKey} from '@metaplex-foundation/umi-web3js-adapters';
+import * as bs58 from "bs58";
 
+const SPL_TOKEN_2022_PROGRAM_ID: PublicKey = publicKey(
+    'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+  );
+
+  
 export function loadWalletKey(keypairFile:string): web3.Keypair {
     const fs = require("fs");
     const loaded = web3.Keypair.fromSecretKey(
@@ -15,56 +21,66 @@ export function loadWalletKey(keypairFile:string): web3.Keypair {
 const INITIALIZE = true;
 
 async function main(){
-    console.log("let's name some tokens!");
-    const myKeypair = loadWalletKey("AndXYwDqSeoZHqk95TUC1pPdp93musGfCo1KztNFNBhd.json");
-    const mint = new web3.PublicKey("FUDMHraEkdj926Tu75aYHtokoHM8QgGN2Nx8TJr8eSZM");
-    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("metadata"));
-    const seed2 = Buffer.from(mpl.PROGRAM_ID.toBytes());
-    const seed3 = Buffer.from(mint.toBytes());
-    const [metadataPDA, _bump] = web3.PublicKey.findProgramAddressSync([seed1, seed2, seed3], mpl.PROGRAM_ID);
-    const accounts = {
-        metadata: metadataPDA,
-        mint,
-        mintAuthority: myKeypair.publicKey,
-        payer: myKeypair.publicKey,
-        updateAuthority: myKeypair.publicKey,
+    console.log("let's name some token-22 tokens in 2024!");
+    const myKeypair = loadWalletKey("/root/.config/solana/id.json");
+    const mint = new web3.PublicKey("CpYsdmwq5nhYk25CyaRvQvmYX7fBrBeuDW7qFMPKYMmZ");
+
+    const umi = createUmi("https://api.mainnet-beta.solana.com");
+    const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
+    umi.use(signerIdentity(signer, true))
+
+    const ourMetadata = { // TODO change those values!
+        name: "FitDog Coin", 
+        symbol: "FITD",
+        uri: "https://raw.githubusercontent.com/GDOGCoin/GDOG/main/metadata.json",
     }
-    const dataV2 = {
-        name: "Fake USD Token",
-        symbol: "FUD",
-        uri: "https://shdw-drive.genesysgo.net/ArP7jjhVZsp7vkzteU7mpKA1fyHRhv4ZBz6gR7MJ1JTC/metadata.json",
-        // we don't need that
-        sellerFeeBasisPoints: 0,
-        creators: null,
-        collection: null,
-        uses: null
-    }
-    let ix;
-    if (INITIALIZE) {
-        const args : mpl.CreateMetadataAccountV3InstructionArgs =  {
-            createMetadataAccountArgsV3: {
-                data: dataV2,
-                isMutable: true,
-                collectionDetails: null
-            }
-        };
-        ix = mpl.createCreateMetadataAccountV3Instruction(accounts, args);
+    if(INITIALIZE){
+        const onChainData = {
+            ...ourMetadata,
+            // we don't need that
+            sellerFeeBasisPoints: percentAmount(0,2),
+            creators: none<Creator[]>(),
+            collection: none<Collection>(),
+            uses: none<Uses>(),
+        }
+        const accounts: UpdateV1InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+            splTokenProgram: SPL_TOKEN_2022_PROGRAM_ID
+        }
+        const data: CreateV1InstructionData = {
+            ...onChainData,
+            isMutable: true,
+            discriminator: 0,
+            tokenStandard: TokenStandard.Fungible,
+            collectionDetails: none<CollectionDetails>(),
+            ruleSet: none<PublicKey>(),
+            createV1Discriminator: 0,
+            primarySaleHappened: true,
+            decimals: none<number>(),
+            printSupply: none<PrintSupply>(),
+        }
+        const txid = await updateV1(umi, {...accounts, ...data}).sendAndConfirm(umi);
+        console.log(bs58.encode(txid.signature))
     } else {
-        const args =  {
-            updateMetadataAccountArgsV2: {
-                data: dataV2,
-                isMutable: true,
-                updateAuthority: myKeypair.publicKey,
-                primarySaleHappened: true
-            }
-        };
-        ix = mpl.createUpdateMetadataAccountV2Instruction(accounts, args)
+        const onChainData = {
+            ...ourMetadata,
+            sellerFeeBasisPoints: 0,
+            creators: none<Creator[]>(),
+            collection: none<Collection>(),
+            uses: none<Uses>(),
+        }
+        const accounts: UpdateV1InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+        }
+        const data = {
+            discriminator: 0,
+            data: some<Data>(onChainData),
+            updateV1Discriminator: 0,
+        }
+        const txid = await updateV1(umi, {...accounts, ...data}).sendAndConfirm(umi);
+        console.log(bs58.encode(txid.signature))
     }
-    const tx = new web3.Transaction();
-    tx.add(ix);
-    const connection = new web3.Connection("https://api.mainnet-beta.solana.com");
-    const txid = await web3.sendAndConfirmTransaction(connection, tx, [myKeypair]);
-    console.log(txid);
+
 
 }
 
